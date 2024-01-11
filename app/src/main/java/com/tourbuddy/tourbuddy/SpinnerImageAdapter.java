7afec0dae24c665931ac45a5cdcf16538050d9ca
noy-dayan <android53789@gmail.com>
2024-01-11 04:48:26 +0200

    added settings

---
+++ app/src/main/java/com/tourbuddy/tourbuddy/SpinnerImageAdapter.java
@@ -0,0 +1,47 @@
+package com.tourbuddy.tourbuddy;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.tourbuddy.tourbuddy.R;
+
+public class SpinnerImageAdapter extends ArrayAdapter<Integer> {
+
+    public SpinnerImageAdapter(Context context, int resource, Integer[] objects) {
+        super(context, resource, objects);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        return getCustomView(position, convertView, parent);
+    }
+
+    @Override
+    public View getDropDownView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        return getCustomView(position, convertView, parent);
+    }
+
+    private View getCustomView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.spinner_language_layout, parent, false);
+        }
+
+        ImageView languageIcon = convertView.findViewById(R.id.languageIcon);
+
+        Integer currentImageResource = getItem(position);
+
+        if (currentImageResource != null) {
+            languageIcon.setImageResource(currentImageResource);
+        }
+
+        return convertView;
+    }
+}
